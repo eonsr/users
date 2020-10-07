7e460adabce84a593a6335f3a45cdc4e4320993c
eonsr <72476048+eonsr@users.noreply.github.com>
2020-10-07 10:15:26 -0500

    Add files via upload

---
+++ frontUser/src/app/DTO/ResponseDTO.ts
@@ -0,0 +1,17 @@
+export class ResponseDTO {
+
+    /**
+     * Estatus de la respuesta
+     */
+    status: boolean;
+
+    /**
+     * Mensaje
+     */
+    msg: string;
+
+    /**
+     * Objeto regresado (podria ser null)
+     */
+    object: object;
+}
---
+++ frontUser/src/app/DTO/UsersDTO.ts
@@ -0,0 +1,32 @@
+export class UsersDTO {
+
+    /**
+     * Id de la columna
+     */
+    id: number ;
+
+    /**
+     * Nombre del Usuario
+     */
+    name: string;
+
+    /**
+     * Apellido Paterno
+     */
+    lastName: string;
+
+    /**
+     * Apellido Materno
+     */
+    motherLastName: string;
+
+    /**
+     * RFC del Usuario
+     */
+    rfc: string;
+
+    /**
+     * Fecha de nacimiento
+     */
+    birthDate: string;
+}
